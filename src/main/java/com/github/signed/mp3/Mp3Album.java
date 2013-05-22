985d17eb239320257ff780b906a8c6851a1af05e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-05-22 22:56:29 +0200

    Collect mp3 files from sub directories

--- src/main/java/com/github/signed/mp3/Mp3Album.java
+++ src/main/java/com/github/signed/mp3/Mp3Album.java
@@ -1,18 +1,19 @@
 package com.github.signed.mp3;
 
-import com.google.common.collect.Lists;
-
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Mp3Album {
 
-    public static Mp3Album For(Path path){
+    public static Mp3Album For(Path path) {
         return new Mp3Album(path);
     }
 
@@ -22,31 +23,48 @@ public class Mp3Album {
         this.path = path;
     }
 
-    public void forEachMp3File(Callback<Context> callback){
-        System.out.println("process album at '"+path+"'");
-        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path, "*.mp3")) {
-            List<Path> allPath = Lists.newArrayList(directoryStream);
-            Collections.sort(allPath, new Comparator<Path>() {
+    public void forEachMp3File(Callback<Context> callback) {
+        System.out.println("process album at '" + path + "'");
+
+        final List<Path> allPath = getMp3s();
+
+        Collections.sort(allPath, new Comparator<Path>() {
+            @Override
+            public int compare(Path o1, Path o2) {
+                return o1.getFileName().compareTo(o2.getFileName());
+            }
+        });
+
+        final int totalNumberOfTracks = allPath.size();
+        int currentTrackNumber = 1;
+
+        for (Path path : allPath) {
+            Mp3 currentMp3 = Mp3.From(path);
+            Context context = new Context(totalNumberOfTracks, currentTrackNumber, path, currentMp3);
+            callback.call(context);
+            ++currentTrackNumber;
+        }
+    }
+
+    private List<Path> getMp3s() {
+        final List<Path> allPath = new ArrayList<>();
+        try {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
                 @Override
-                public int compare(Path o1, Path o2) {
-                    return o1.getFileName().compareTo(o2.getFileName());
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.getFileName().toString().endsWith(".mp3")) {
+                        allPath.add(file);
+                    }
+                    return super.visitFile(file, attrs);    //To change body of overridden methods use File | Settings | File Templates.
                 }
             });
-
-            final int totalNumberOfTracks = allPath.size();
-            int currentTrackNumber = 1;
-
-            for (Path path : allPath) {
-                Mp3 currentMp3 = Mp3.From(path);
-                Context context = new Context(totalNumberOfTracks, currentTrackNumber, path, currentMp3);
-                callback.call(context);
-                ++currentTrackNumber;
-            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return allPath;
     }
-    public static class Context{
+
+    public static class Context {
         public final int totalNumberOfTracks;
         public final int trackNumber;
         public final Path currentTracksPath;
