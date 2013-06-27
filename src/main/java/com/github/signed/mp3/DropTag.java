84282b82cc6b6947393489d29e11322903d185fd
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2013-06-27 13:16:24 +0200

    Static creation method for drop tag

--- src/main/java/com/github/signed/mp3/DropTag.java
+++ src/main/java/com/github/signed/mp3/DropTag.java
@@ -1,6 +1,11 @@
 package com.github.signed.mp3;
 
 public class DropTag implements Callback<Mp3Album.Context> {
+
+    public Callback<Mp3Album.Context> drop(Tag tag) {
+        return new DropTag(tag);
+    }
+
     private final Tag tag;
 
     public DropTag(Tag tag) {
